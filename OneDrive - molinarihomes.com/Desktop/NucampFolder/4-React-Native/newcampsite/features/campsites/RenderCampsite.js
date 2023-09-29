4fa9e3e572d3312158cd45614cf5aecfcdb852e8
Chris Molinari <cmolinari825@gmail.com>
2023-09-29 01:23:17 -0700

    Gestures Part 2

--- OneDrive - molinarihomes.com/Desktop/NucampFolder/4-React-Native/newcampsite/features/campsites/RenderCampsite.js
+++ OneDrive - molinarihomes.com/Desktop/NucampFolder/4-React-Native/newcampsite/features/campsites/RenderCampsite.js
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import { StyleSheet, Text, View, PanResponder, Alert } from "react-native";
 import { Card, Icon } from "react-native-elements";
 import { baseUrl } from "../../shared/baseUrl";
@@ -6,10 +7,19 @@ import * as Animatable from "react-native-animatable";
 const RenderCampsite = (props) => {
   const { campsite } = props;
 
+  const view = useRef();
+
   const isLeftSwipe = ({ dx }) => dx < -200;
 
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
+    onPanResponderGrant: () => {
+      view.current
+        .rubberBand(1000)
+        .then((endState) =>
+          console.log(endState.finished ? "finished" : "canceled")
+        );
+    },
     onPanResponderEnd: (e, gestureState) => {
       console.log("pan responder end", gestureState);
       if (isLeftSwipe(gestureState)) {
@@ -35,12 +45,14 @@ const RenderCampsite = (props) => {
       }
     },
   });
+
   if (campsite) {
     return (
       <Animatable.View
         animation="fadeInDownBig"
         duration={2000}
         delay={1000}
+        ref={view}
         {...panResponder.panHandlers}
       >
         <Card containerStyle={styles.cardContainer}>
@@ -50,7 +62,6 @@ const RenderCampsite = (props) => {
             </View>
           </Card.Image>
           <Text style={{ margin: 20 }}>{campsite.description}</Text>
-
           <View style={styles.cardRow}>
             <Icon
               name={props.isFavorite ? "heart" : "heart-o"}
@@ -77,7 +88,6 @@ const RenderCampsite = (props) => {
       </Animatable.View>
     );
   }
-
   return <View />;
 };
 
