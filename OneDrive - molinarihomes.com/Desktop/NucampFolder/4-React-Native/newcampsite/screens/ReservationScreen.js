2054b02d855ac5d77913a1d26cc741938b6b69c3
Chris Molinari <cmolinari825@gmail.com>
2023-10-04 22:15:16 -0700

    Local Notifications

--- OneDrive - molinarihomes.com/Desktop/NucampFolder/4-React-Native/newcampsite/screens/ReservationScreen.js
+++ OneDrive - molinarihomes.com/Desktop/NucampFolder/4-React-Native/newcampsite/screens/ReservationScreen.js
@@ -11,13 +11,13 @@ import {
 import { Picker } from "@react-native-picker/picker";
 import DateTimePicker from "@react-native-community/datetimepicker";
 import * as Animatable from "react-native-animatable";
+import * as Notifications from "expo-notifications";
 
 const ReservationScreen = () => {
   const [campers, setCampers] = useState(1);
   const [hikeIn, setHikeIn] = useState(false);
   const [date, setDate] = useState(new Date());
   const [showCalendar, setShowCalendar] = useState(false);
-  // const [showModal, setShowModal] = useState(false);
 
   const onDateChange = (event, selectedDate) => {
     const currentDate = selectedDate || date;
@@ -26,26 +26,34 @@ const ReservationScreen = () => {
   };
 
   const handleReservation = () => {
-    console.log("campers:", campers);
-    console.log("hikeIn:", hikeIn);
-    console.log("date:", date);
+    const message = `Number of Campers: ${campers}
+                            \nHike-In? ${hikeIn}
+                            \nDate: ${date.toLocaleDateString("en-US")}`;
     Alert.alert(
       "Begin Search?",
-      `Number of Campers: ${campers} \n \n Hike-In? ${hikeIn} \n \n Date: ${date.toLocaleDateString(
-        "en-US"
-      )}`,
+      message,
       [
         {
           text: "Cancel",
-          onPress: () => resetForm(),
+          onPress: () => {
+            console.log("Reservation Search Canceled");
+            resetForm();
+          },
           style: "cancel",
         },
         {
           text: "OK",
-          onPress: () => resetForm(),
+          onPress: () => {
+            presentLocalNotification(date.toLocaleDateString("en-US"));
+            resetForm();
+          },
         },
-      ]
+      ],
+      { cancelable: false }
     );
+    console.log("campers:", campers);
+    console.log("hikeIn:", hikeIn);
+    console.log("date:", date);
   };
 
   const resetForm = () => {
@@ -53,6 +61,34 @@ const ReservationScreen = () => {
     setHikeIn(false);
     setDate(new Date());
     setShowCalendar(false);
+  };
+
+  const presentLocalNotification = async (reservationDate) => {
+    const sendNotification = () => {
+      Notifications.setNotificationHandler({
+        handleNotification: async () => ({
+          shouldShowAlert: true,
+          shouldPlaySound: true,
+          shouldSetBadge: true,
+        }),
+      });
+
+      Notifications.scheduleNotificationAsync({
+        content: {
+          title: "Your Campsite Reservation Search",
+          body: `Search for ${reservationDate} requested`,
+        },
+        trigger: null,
+      });
+    };
+
+    let permissions = await Notifications.getPermissionsAsync();
+    if (!permissions.granted) {
+      permissions = await Notifications.requestPermissionsAsync();
+    }
+    if (permissions.granted) {
+      sendNotification();
+    }
   };
 
   return (
@@ -108,31 +144,6 @@ const ReservationScreen = () => {
             accessibilityLabel="Tap me to search for available campsites to reserve"
           />
         </View>
-        {/* <Modal
-          animationType="slide"
-          transparent={false}
-          visible={showModal}
-          onRequestClose={() => setShowModal(!showModal)}
-        >
-          <View style={styles.modal}>
-            <Text style={styles.modalTitle}>Search Campsite Reservations</Text>
-            <Text style={styles.modalText}>Number of Campers: {campers}</Text>
-            <Text style={styles.modalText}>
-              Hike-In?: {hikeIn ? "Yes" : "No"}
-            </Text>
-            <Text style={styles.modalText}>
-              Date: {date.toLocaleDateString("en-US")}
-            </Text>
-            <Button
-              onPress={() => {
-                setShowModal(!showModal);
-                resetForm();
-              }}
-              color="#5637DD"
-              title="Close"
-            />
-          </View>
-        </Modal> */}
       </Animatable.View>
     </ScrollView>
   );
@@ -153,52 +164,6 @@ const styles = StyleSheet.create({
   formItem: {
     flex: 1,
   },
-
-  //   modal: {
-  //     justifyContent: "center",
-  //     margin: 20,
-  //   },
-  //   modalTitle: {
-  //     fontSize: 24,
-  //     fontWeight: "bold",
-  //     backgroundColor: "#5637DD",
-  //     textAlign: "center",
-  //     color: "#fff",
-  //     marginBottom: 20,
-  //   },
-  //   modalText: {
-  //     fontSize: 18,
-  //     margin: 10,
 });
 
 export default ReservationScreen;
-
-// const App = () => {
-//   const createTwoButtonAlert = () =>
-//     Alert.alert(
-//       'Begin Search?',
-//       'Number of Campers:' {campers},
-//       'Hike-In?:' {hikeIn ? "Yes" : "No"},
-//       'Date:' {date.toLocaleDateString("en-US")}
-//       [
-//       {
-//         text: 'Cancel',
-//         onPress: () => console.log('Cancel Pressed'),
-//         style: 'cancel',
-//       },
-//       {text: 'OK', onPress: () => console.log('OK Pressed')},
-//     ]);
-
-//   return (
-//     <View style={styles.container}>
-//       <Button title={'2-Button Alert'} onPress={createTwoButtonAlert} />
-//     </View>
-//   );
-// };
-
-// container: {
-//   flex: 1,
-//   justifyContent: 'space-around',
-//   alignItems: 'center',
-// },
-// });
